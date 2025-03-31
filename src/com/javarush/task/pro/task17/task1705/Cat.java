a6aacba64001c9044393a497481647d001ffe3ae
AlikDnepr <oleh.shlykov@gmail.com>
2025-03-31 15:52:59 +0300

    new solutions

---
+++ src/com/javarush/task/pro/task17/task1705/Cat.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task17.task1705;
+
+import java.util.ArrayList;
+
+public class Cat extends Astronaut {
+    public String getInfo() {
+        public static ArrayList<Astronaut> astronauts = new ArrayList<>();
+        for (int i = 0; i < astronauts.size(); i++) {
+
+        }
+        return "Кот";
+    }
+
+}
